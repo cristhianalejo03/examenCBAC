0945ec727cd86014868cb576e71b30bdccc17b6b
cristhianalejo03 <cristhianalejocastillo03@gmail.com>
2022-05-13 22:22:38 +0000

    cris

--- CBAC202012374/examen.java
+++ CBAC202012374/examen.java
@@ -1,24 +1,66 @@
 import java.util.Scanner;
+
 public class examen{
-    static Scanner lt=new Scanner(System.in);
+
+    static Scanner it=new Scanner(System.in);
+    private static int x;
     public static void main(String[] args) {
-        //Declarar Variables
-        int edad;
+        //declarar variables
+        int nivelavanzado=4;
+        int nivelregular=3;
+        int nivelbasico=2;
+        int nivel=1;
+        int notafinal=x;
+        x=11;
+        float nota;
+        float nota1;
+        float nota2;
+        float nota3;
+        float notacono;
+        float notaentrepersonal;
+        float notatespsicologico;
         String mensaje;
-        //Datos de entrada
-        System.out.println("Ingrese la edad:");
-        edad=lt.nextInt();
-        //Proceso
-        if (edad>=18) {
-            mensaje="Puede votar porque su edad es "+edad;
-        } else {
-            mensaje="No puede votar porque su edad es "+edad;
+        //datos de entrada
+        System.out.println("Ingrese la nota1:");
+        nota1=it.nextInt();
+        System.out.println("Ingrese la nota2:");
+        nota2=it.nextInt();
+        System.out.println("Ingrese la nota3:");
+        nota3=it.nextInt();
+        // datos de proceso
+        if (17 <= notafinal ){
+            notacono=((float) (nota1 * 0.4));
+            notaentrepersonal=((float) (nota2*0.35));
+            notatespsicologico=(float) (nota3*0.25);
+            notafinal=(int) (notacono+notaentrepersonal+notatespsicologico);
+            mensaje="notafinal" +notafinal;
         }
-        //Datos de Salida
-        System.out.println(mensaje);        
+        if (notafinal <= 17 && notafinal > 14) {
+            notacono=(float) (nota1*0.4);
+            notaentrepersonal=(float) (nota2*0.35);
+            notatespsicologico=(float) (nota3*0.25);
+            notafinal=(int) (notacono+notaentrepersonal+notatespsicologico);
+            mensaje="notafinal" +notafinal;
+        }
+        else if (notafinal < 14 && notafinal >= 11);
+            notacono=(float) (nota1*0.4);
+            notaentrepersonal=(float) (nota2*0.35);
+            notatespsicologico=(float) (nota3*0.25);
+            notafinal=(int) (notacono+notaentrepersonal+notatespsicologico);
+            mensaje="notafinal" +notafinal;
+    
+        if (notafinal >= 11) {
+            notacono=(float) (nota1*0.4);
+            notaentrepersonal=(float) (nota2*0.35);
+            notatespsicologico=(float) (nota3*0.25);
+            notafinal=(int) (notacono+notaentrepersonal+notatespsicologico);
+            mensaje="notafinal" +notafinal;
+        }
+
+        //datos de salida
+        System.out.println("nota final"+notafinal);
+      
+
+        
     }
 }
-
-
-
- 
